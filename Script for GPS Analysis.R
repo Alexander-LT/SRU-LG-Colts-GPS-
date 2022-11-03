29a6684b284945c87234fcab2188255b68178807
Alexander-LT <111735234+Alexander-LT@users.noreply.github.com>
2022-11-04 08:41:44 +1100

    AT - 31.10.22
    Loaded dataset.

--- Script for GPS Analysis.R
+++ Script for GPS Analysis.R
@@ -1,3 +1,24 @@
 
 # Load Packages 
 library(tidyverse)
+library(readxl)
+library(lubridate)
+
+# Load data 
+RawData <- read.csv("Data/2022SeasonGPS.csv", header = TRUE, stringsAsFactors = FALSE)
+
+# assess structure of the rawdata
+str(RawData)
+
+# edit the date column so that they are readable dates 
+RawData %>% 
+  group_by(Player.Name) %>% 
+  mutate(Date = ymd(Date))
+
+GPSData <- RawData %>% 
+  ymd(RawData$Date)
+  
+
+# Edit column names for better analysis
+GPSSummary <- RawData %>% 
+  select()
