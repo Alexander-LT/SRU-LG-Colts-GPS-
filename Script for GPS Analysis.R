787e5fcd4d5789dda3f3eb90c58b9a107cb74abc
Alexander-LT <111735234+Alexander-LT@users.noreply.github.com>
2022-11-04 14:34:37 +1100

    Dates - AT - 04112022
    Have converted date columns.

--- Script for GPS Analysis.R
+++ Script for GPS Analysis.R
@@ -1,23 +1,30 @@
 
 # Load Packages 
 library(tidyverse)
-library(readxl)
 library(lubridate)
 
+
 # Load data 
-RawData <- read.csv("Data/2022SeasonGPS.csv", header = TRUE, stringsAsFactors = FALSE)
+RawData <- read.csv("Data/2022SeasonGPS.csv", header = TRUE, stringsAsFactors = TRUE)
 
 # assess structure of the rawdata
 str(RawData)
 
 # edit the date column so that they are readable dates 
-RawData %>% 
-  group_by(Player.Name) %>% 
-  mutate(Date = ymd(Date))
+RawData$Date <- as.Date(RawData$Date, origin = '1899-12-30')
+RawData$Split.Start.Time <- as.Date(RawData$Split.Start.Time, origin = '1899-12-30')
+class(RawData$Date) # Check the class, make sure it is date 
+RawData$Date <- as.POSIXct(as.Date(RawData$Date, origin = '1899-12-30'))
+RawData$Split.Start.Time <- as.POSIXct(as.Date(RawData$Split.Start.Time, origin = '1899-12-30'))
+RawData$Split.End.Time <- as.POSIXct(as.Date(RawData$Split.End.Time, origin = '1899-12-30'))
+class(RawData$Split.Start.Time)
+# Add a days column and a month column 
+RawData$Day <- weekdays(as.Date(RawData$Date))
+RawData$Month <- months(as.Date(RawData$Date))
+# Split the date, split start and end columns to have time only 
 
-GPSData <- RawData %>% 
-  ymd(RawData$Date)
-  
+RawData$Hours <- format(as.POSIXct(RawData$Date, "%D-%M-%y", tz = "", format = "%H:%M:%S"))
+
 
 # Edit column names for better analysis
 GPSSummary <- RawData %>% 
