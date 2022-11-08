7a91974e17bc25529d61cc9d01cad05d4cd45d46
Alexander-LT <111735234+Alexander-LT@users.noreply.github.com>
2022-11-09 10:44:10 +1100

    Began Tidy
    Started tidying the date getting ready for viz

--- Script for GPS Analysis.R
+++ Script for GPS Analysis.R
@@ -2,30 +2,34 @@
 # Load Packages 
 library(tidyverse)
 library(lubridate)
+library(DT)
 
 
 # Load data 
-RawData <- read.csv("Data/2022SeasonGPS.csv", header = TRUE, stringsAsFactors = TRUE)
+RawData <- read.csv("Data/2022SeasonGPS.csv")
+                    
+                    #header = TRUE, stringsAsFactors = TRUE)
 
 # assess structure of the rawdata
 str(RawData)
 
+class(RawData$Date)
 # edit the date column so that they are readable dates 
-RawData$Date <- as.Date(RawData$Date, origin = '1899-12-30')
-RawData$Split.Start.Time <- as.Date(RawData$Split.Start.Time, origin = '1899-12-30')
-class(RawData$Date) # Check the class, make sure it is date 
-RawData$Date <- as.POSIXct(as.Date(RawData$Date, origin = '1899-12-30'))
-RawData$Split.Start.Time <- as.POSIXct(as.Date(RawData$Split.Start.Time, origin = '1899-12-30'))
-RawData$Split.End.Time <- as.POSIXct(as.Date(RawData$Split.End.Time, origin = '1899-12-30'))
-class(RawData$Split.Start.Time)
-# Add a days column and a month column 
-RawData$Day <- weekdays(as.Date(RawData$Date))
-RawData$Month <- months(as.Date(RawData$Date))
-# Split the date, split start and end columns to have time only 
+RawData$Date <- dmy(RawData$Date)
+RawData$Date <- format(RawData$Date, format = "%d/%m/%y")
 
-RawData$Hours <- format(as.POSIXct(RawData$Date, "%D-%M-%y", tz = "", format = "%H:%M:%S"))
+# Tidy the tibble 
+RawData_Tidy <- RawData %>% 
+  select(Date, Session.Title, Tags, Split.Name, Split.Start.Time, Split.End.Time, Player.Name, Distance..metres., Distance.Per.Min..m.min., Top.Speed..m.s.,
+         Sprint.Distance..m., Duration, Distance.in.Speed.Zone.3...metres., 
+         Distance.in.Speed.Zone.4...metres., Distance.in.Speed.Zone.5...metres.) %>% 
+  rename(Athlete = Player.Name, Distance = Distance..metres., TopSpeed = Top.Speed..m.s., MetresPerMin = Distance.Per.Min..m.min.) %>% 
+  mutate(Duration_mins = round(Duration / 60, 1),  
+         HSR = round(Distance.in.Speed.Zone.3...metres. + Distance.in.Speed.Zone.4...metres. + Distance.in.Speed.Zone.5...metres., 1)) %>% 
+  select(-c(Duration, Distance.in.Speed.Zone.3...metres. , Distance.in.Speed.Zone.4...metres. , Distance.in.Speed.Zone.5...metres.))
+
+ggplot(RawData_Tidy, aes(x = Date, y = Distance, colour = Athlete)) +
+  geom_point() +
+  facet_wrap(~Athlete)
 
 
-# Edit column names for better analysis
-GPSSummary <- RawData %>% 
-  select()
