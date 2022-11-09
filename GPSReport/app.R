b99739f1a812b62935b54bff8055019b101e0ecd
Alexander-LT <111735234+Alexander-LT@users.noreply.github.com>
2022-11-09 20:41:57 +1100

    Shiny App
    Created Shiny App

---
+++ GPSReport/app.R
@@ -0,0 +1,164 @@
+# Load Packages 
+library(shiny)
+library(tidyverse)
+library(viridis)
+library(plotly)
+library(bslib)
+library(DT)
+
+# Load data
+RawData <- read.csv("Data/2022SeasonGPS.csv")
+# Format dates 
+RawData$Date <- ymd(RawData$Date)
+# Add month column
+RawData$Month <- month(ymd(RawData$Date))
+
+# Tidy data 
+
+RawData_Tidy <- RawData %>% 
+    select(Date, Month, Day, SessionDate, Session.Title, Tags, Split.Name, Split.Start.Time, Split.End.Time, Player.Name, Distance..metres., Distance.Per.Min..m.min., Top.Speed..m.s.,
+           Sprint.Distance..m., Duration, Distance.in.Speed.Zone.3...metres., 
+           Distance.in.Speed.Zone.4...metres., Distance.in.Speed.Zone.5...metres.) %>% 
+    rename(Athlete = Player.Name, 
+           Distance = Distance..metres., 
+           TopSpeed = Top.Speed..m.s., 
+           MetresPerMin = Distance.Per.Min..m.min., 
+           Tag = Tags, SessionTitle = Session.Title,
+           SpintDistance = Sprint.Distance..m.) %>% 
+    mutate(Duration_mins = round(Duration / 60, 1),  
+           HSR = round(Distance.in.Speed.Zone.3...metres. + Distance.in.Speed.Zone.4...metres. + Distance.in.Speed.Zone.5...metres., 1),
+           Distance = round(Distance, 1), 
+           MetresPerMin = round(MetresPerMin, 1), 
+           TopSpeed = round(TopSpeed, 1),
+           SpintDistance = round(SpintDistance, 1)) %>% 
+    select(-c(Duration, Distance.in.Speed.Zone.3...metres., Distance.in.Speed.Zone.4...metres. , Distance.in.Speed.Zone.5...metres.)) %>% 
+    na.omit() 
+
+
+
+# Define UI for application - can change title below, to add in team name
+ui <- navbarPage("Example GPS Report",
+                 # Add navbar - can change name (to "By Athlete", for example)
+                 tabPanel("By Athlete", 
+                          # Create a dropdown selection for individual athlete analysis
+                          selectInput("Athlete", label = h4("Select Athlete:"), 
+                                      choices = unique(RawData_Tidy$Athlete), 
+                                      selected = 1),
+                          # Plot output (code below in server)
+                          mainPanel(plotOutput("plot1"), width = "100%", height = "100%")),
+                 # Repeat for session/ by date analysis
+                 tabPanel("Session Analysis", 
+                          selectInput("SessionDate1", label = h3("Select Session Date:"), 
+                                      choices = unique(RawData_Tidy$SessionDate), 
+                                      selected = 1),
+                          # Create plot output
+                          mainPanel(plotOutput("plot2"), width = "100%", height = "100%")), 
+                 # Repeat for session/ by date analysis (interactive)
+                 tabPanel("Session Analysis (Interactive)", 
+                          selectInput("SessionDate2", label = h3("Select Session Date:"), 
+                                      choices = unique(RawData_Tidy$SessionDate), 
+                                      selected = 1),
+                          # Create plot output
+                          mainPanel(plotlyOutput("plot3"), width = "100%", height = "100%")), 
+                 # Repeat for session type
+                 tabPanel("Session Type Analysis (Interactive)", 
+                          selectInput("SessionType", label = h3("Select Session Type:"), 
+                                      choices = unique(RawData_Tidy$Split.Name), 
+                                      selected = 1),
+                          # Create plot output
+                          mainPanel(plotlyOutput("plot4"), width = "100%", height = "100%")), 
+                 # Present all raw data in a table
+                 tabPanel("All Data", 
+                          DT::dataTableOutput("AllDataTable")),
+                 theme = bs_theme(version = 4, bootswatch = "cosmo"))
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    output$plot1 <- renderPlot({
+        SelectedAthlete <- RawData_Tidy[RawData_Tidy$Athlete==input$AthleteName,] 
+        ggplot(data = SelectedAthlete, aes(x = Date, y = Value)) + 
+            geom_line(linetype = "dashed", colour = "grey") +
+            geom_point(aes(colour = Split.Name), size = 4) +
+            labs(x = NULL, y = NULL) +
+            scale_x_date(breaks = unique(SelectedAthlete$Date), date_labels = "%b %d") +
+            scale_colour_viridis_d() +
+            theme_classic() +
+            theme(axis.title.x = element_blank(), 
+                  axis.text.x = element_text(size = 9, angle = 45, vjust = 1, hjust = 1, colour = "black"),
+                  axis.text.y = element_text(size = 10, colour = "black"), 
+                  strip.text.x = element_text(size = 12, face = "bold"),
+                  panel.spacing = unit(2, "lines"), 
+                  legend.position = "bottom", 
+                  legend.title = element_blank()) +
+            facet_wrap(~Variable, scales = "free")
+    }, height = 900)
+    
+    output$plot2 <- renderPlot({
+        SelectedDate <- RawData_Tidy[RawData_Tidy$SessionDate==input$SessionDate1,] 
+        ggplot(data = SelectedDate, aes(x = Variable, y = Value)) + 
+            geom_boxplot(alpha = 0.7, colour = "lightgrey", outlier.shape = NA) +
+            geom_jitter(aes(colour = Athlete), size = 4) +
+            labs(x = NULL, y = NULL) +
+            scale_colour_viridis_d(option = "plasma") +
+            theme_classic() +
+            theme(axis.title.x = element_blank(), 
+                  axis.text.x = element_blank(),
+                  axis.ticks.x = element_blank(),
+                  axis.text.y = element_text(size = 10, colour = "black"), 
+                  strip.text.x = element_text(size = 12, face = "bold"),
+                  panel.spacing = unit(2, "lines"), 
+                  legend.position = "bottom", 
+                  legend.title = element_blank()) +
+            facet_wrap(~Variable, scales = "free")
+    }, height = 900)
+    
+    output$plot3 <- renderPlotly({
+        SelectedDate2 <- RawData_Tidy[RawData_Tidy$SessionDate==input$SessionDate2,] 
+        print(
+            ggplotly(
+                ggplot(data = SelectedDate2, aes(x = Variable, y = Value)) + 
+                    geom_boxplot(alpha = 0.7, colour = "lightgrey", outlier.shape = NA) +
+                    geom_jitter(aes(colour = Athlete), size = 4) +
+                    labs(x = NULL, y = NULL) +
+                    scale_colour_viridis_d(option = "plasma") +
+                    theme_classic() +
+                    theme(axis.title.x = element_blank(), 
+                          axis.text.x = element_blank(),
+                          axis.ticks.x = element_blank(),
+                          axis.text.y = element_text(size = 10, colour = "black"), 
+                          strip.text.x = element_text(size = 12, face = "bold"),
+                          panel.spacing = unit(2, "lines"), 
+                          legend.position = "bottom", 
+                          legend.title = element_blank()) +
+                    facet_wrap(~Variable, scales = "free")) %>% layout(height = 1200, width = 2000))
+    })
+    
+    output$plot4 <- renderPlotly({
+        SelectedSessionType <- RawData_Tidy[RawData_Tidy$Split.Name==input$SessionType,] 
+        print(
+            ggplotly(
+                ggplot(data = SelectedSessionType, aes(x = Variable, y = Value, label = Date)) + 
+                    geom_violin(alpha = 0.7) +
+                    stat_summary(fun=median, geom="crossbar", size=2, color="darkgrey") +
+                    geom_jitter(aes(colour = Athlete), size = 4) +
+                    labs(x = NULL, y = NULL) +
+                    scale_colour_viridis_d(option = "plasma") +
+                    theme_classic() +
+                    theme(axis.title.x = element_blank(), 
+                          axis.text.x = element_blank(),
+                          axis.ticks.x = element_blank(),
+                          axis.text.y = element_text(size = 10, colour = "black"), 
+                          strip.text.x = element_text(size = 12, face = "bold"),
+                          panel.spacing = unit(2, "lines"), 
+                          legend.position = "bottom", 
+                          legend.title = element_blank()) +
+                    facet_wrap(~Variable, scales = "free")) %>% layout(height = 1200, width = 2000))
+    })
+    
+    output$AllDataTable = DT::renderDataTable({
+        RawData_Tidy 
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
--- Script for GPS Analysis.R
+++ Script for GPS Analysis.R
@@ -1,35 +1,51 @@
-
+# Section 1 ####
 # Load Packages 
 library(tidyverse)
 library(lubridate)
 library(DT)
 
-
+# Section 2 ####
 # Load data 
-RawData <- read.csv("Data/2022SeasonGPS.csv")
-                    
-                    #header = TRUE, stringsAsFactors = TRUE)
+RawData <- read.csv("Data/2022SeasonGPS.csv") #header = TRUE, stringsAsFactors = TRUE) # Add this in to make each column factored
 
 # assess structure of the rawdata
 str(RawData)
 
 class(RawData$Date)
+# Section 3 ####
+# Tidying 
 # edit the date column so that they are readable dates 
-RawData$Date <- dmy(RawData$Date)
-RawData$Date <- format(RawData$Date, format = "%d/%m/%y")
+RawData$Date <- ymd(RawData$Date) # This turns the date column into date format
+# Add a month column to help with analyis
+RawData$Month <- month(ymd(RawData$Date))
+# RawData$Date <- format(RawData$Date, format = "%d/%m/%y") # Dehash this for character date
+
+# Unite session title and dat e
+RawData <- RawData %>%
+  unite(SessionDate, "Date":"Session.Title", sep = " - ", remove = FALSE)
 
 # Tidy the tibble 
+# Select the columns we want for analysis. Rename for easier call up. Mutate new columns for new variables i.e. Raw Duration is in seconds
+# so convert to mins. # remove the columns we no longer need after the mutation. 
 RawData_Tidy <- RawData %>% 
-  select(Date, Session.Title, Tags, Split.Name, Split.Start.Time, Split.End.Time, Player.Name, Distance..metres., Distance.Per.Min..m.min., Top.Speed..m.s.,
+  select(Date, Month, Day, SessionDate, Session.Title, Tags, Split.Name, Split.Start.Time, Split.End.Time, Player.Name, Distance..metres., Distance.Per.Min..m.min., Top.Speed..m.s.,
          Sprint.Distance..m., Duration, Distance.in.Speed.Zone.3...metres., 
          Distance.in.Speed.Zone.4...metres., Distance.in.Speed.Zone.5...metres.) %>% 
-  rename(Athlete = Player.Name, Distance = Distance..metres., TopSpeed = Top.Speed..m.s., MetresPerMin = Distance.Per.Min..m.min.) %>% 
+  rename(Athlete = Player.Name, 
+         Distance = Distance..metres., 
+         TopSpeed = Top.Speed..m.s., 
+         MetresPerMin = Distance.Per.Min..m.min., 
+         Tag = Tags, SessionTitle = Session.Title,
+         SpintDistance = Sprint.Distance..m.) %>% 
   mutate(Duration_mins = round(Duration / 60, 1),  
-         HSR = round(Distance.in.Speed.Zone.3...metres. + Distance.in.Speed.Zone.4...metres. + Distance.in.Speed.Zone.5...metres., 1)) %>% 
-  select(-c(Duration, Distance.in.Speed.Zone.3...metres. , Distance.in.Speed.Zone.4...metres. , Distance.in.Speed.Zone.5...metres.))
+         HSR = round(Distance.in.Speed.Zone.3...metres. + Distance.in.Speed.Zone.4...metres. + Distance.in.Speed.Zone.5...metres., 1),
+         Distance = round(Distance, 1), 
+         MetresPerMin = round(MetresPerMin, 1), 
+         TopSpeed = round(TopSpeed, 1),
+         SpintDistance = round(SpintDistance, 1)) %>% 
+  select(-c(Duration, Distance.in.Speed.Zone.3...metres., Distance.in.Speed.Zone.4...metres. , Distance.in.Speed.Zone.5...metres.)) %>% 
+  na.omit() 
+  
 
-ggplot(RawData_Tidy, aes(x = Date, y = Distance, colour = Athlete)) +
-  geom_point() +
-  facet_wrap(~Athlete)
 
 
